da3d204427a88f5e90d05060c89ed292a70bbbaf
ashenafiDL <ashenafi.debella101@gmail.com>
2025-03-23 12:19:19 +0300

    Added day 05/2024 solutions

---
+++ 2024/day-05/day-05.py
@@ -0,0 +1,59 @@
+import networkx as nx
+
+
+def check_if_correctly_ordered(graph, pages):
+    """Check if the give list of pages in correct order"""
+
+    sub_graph = nx.subgraph(graph, pages)
+    topo_sort = list(nx.topological_sort(sub_graph))
+
+    return topo_sort == pages
+
+
+def corrected_order(graph, pages):
+    """Return the correct order of the pages"""
+
+    sub_graph = nx.subgraph(graph, pages)
+    correct_order = list(nx.topological_sort(sub_graph))
+
+    return correct_order
+
+
+def get_middle_page(pages) -> int:
+    """Return the middle page of the given list of pages"""
+
+    middle = len(pages) // 2
+    return int(pages[middle])
+
+
+def main():
+    with open("2024/day-05/day-05-input.txt", "r") as f:
+        lines = f.readlines()
+        separator_index = lines.index("\n")
+        rules = lines[:separator_index]
+        updates = lines[separator_index + 1 :]
+
+        # Created a directed graph for rules
+        G = nx.DiGraph()
+        for rule in rules:
+            rule = rule.strip().split("|")
+            G.add_node(rule[0])
+            G.add_node(rule[1])
+            G.add_edge(rule[0], rule[1])
+
+        middle_sum = 0
+        middle_sum_for_incorrect_updates = 0
+        for update in updates:
+            pages = update.strip().split(",")
+            if check_if_correctly_ordered(G, pages):
+                middle_sum += get_middle_page(pages)
+            else:
+                correct_order = corrected_order(G, pages)
+                middle_sum_for_incorrect_updates += get_middle_page(correct_order)
+
+        print("Middle sum:", middle_sum)
+        print("Middle sum for incorrect updates:", middle_sum_for_incorrect_updates)
+
+
+if __name__ == "__main__":
+    main()
